f975290d69d5790f8f9f7d6e1e37d10dceacee08
Wth10 <weuberth10@gmail.com>
2022-06-14 00:01:00 -0300

    FileUpdate

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,35 +1,20 @@
 import Script from 'next/script';
 import Browser from '../components/Browser';
-import Head from '../layout/Head';
 
-export default function Index() {
+export default function Home() {
 	return (
 		<>
 			<header>
-				<title>Next-App</title>
+				<title>Welcome-Next-App</title>
 				<Script src="https://cdn.tailwindcss.com" strategy="beforeInteractive" />
 				<meta charset="UTF-8" />
 				<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 			</header>
 			<main>
-				<Head />
-			
-				<div className="bg-[#00FF7F]  text-[#00008B]">
-					<span className="p-12 m-10">
-						<Browser name="Page Two" destiny="/home" />
-					</span>
-					<span className="p-12 m-10">
-						<Browser name="API" destiny="/api/hello" />
-					</span>
-					<span className="p-12 m-10">
-						<Browser name="Null" destiny="/api/user/user" />
-					</span>
-				</div>
-			
 				<div className="bg-white dark:bg-slate-800 space-y-4 p-4 sm:px-8 sm:py-6 lg:p-4 xl:px-8 xl:py-6">
 					<div className="flex items-center justify-between">
 						<h2 className="font-semibold text-white">Projects</h2>
-						<a href="/new" className="hover:bg-blue-400 group flex items-center rounded-md bg-blue-500 text-white text-sm font-medium pl-2 pr-3 py-2 shadow-sm">
+						<a href="" className="hover:bg-blue-400 group flex items-center rounded-md bg-blue-500 text-white text-sm font-medium pl-2 pr-3 py-2 shadow-sm">
 							<svg width="20" height="20" fill="currentColor" className="mr-2" aria-hidden="true">
 								<path d="M10 5a1 1 0 0 1 1 1v3h3a1 1 0 1 1 0 2h-3v3a1 1 0 1 1-2 0v-3H6a1 1 0 1 1 0-2h3V6a1 1 0 0 11-1Z" />
 							</svg>
@@ -37,10 +22,10 @@ export default function Index() {
 						</a>
 					</div>
 					<form className="group relative">
-						<svg width="20" height="20" fill="currentColor" className="absolute left-3 top-1/2 -mt-2.5 text-slate-400 pointer-events-none group-focus-within:text-blue-500" aria-hidden="true">
+						<svg width="20" height="20" className="absolute left-3 top-1/2 -mt-2.5 text-slate-400 pointer-events-none group-focus-within:text-blue-500" aria-hidden="true">
 							<path fillRule="evenodd" clipRule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" />
 						</svg>
-						<input className="focus:ring-2 focus:ring-blue-500 focus:outline-none appearance-none w-full text-sm leading-6 text-slate-900 placeholder-slate-400 rounded-md py-2 pl-10 ring-1 ring-slate-200 shadow-sm" type="text" aria-label="Filter projects" placeholder="Filter projects..." />
+						<input className="focus:ring-2 focus:ring-blue-500 focus:outline-none appearance-none w-full text-sm leading-6 text-slate-900 placeholder-slate-400 rounded-md py-2 pl-10 ring-1 ring-slate-200 shadow-sm" type="text" placeholder="Filter projects..." />
 					</form>
 				</div>
 
@@ -52,6 +37,24 @@ export default function Index() {
 							over me. I don't know if it was divine intervention or the kinship of all living
 							things but I tell you Jerry at that moment, I <em>was</em> a marine biologist.
 						</p>
+					</div>
+
+					<div className="flex flex-row space-x-4 font-mono text-white text-sm font-bold leading-6">
+						<div className='w-10 h-10 rounded-lg flex items-center justify-center bg-fuchsia-500 shadow-lg'>
+							<span>
+								<Browser name="One" destiny="/home" />
+							</span>
+						</div>
+						<div className='w-10 h-10 rounded-lg flex items-center justify-center bg-fuchsia-500 shadow-lg'>
+							<span>
+								<Browser name="Two" destiny="/" />
+							</span>
+						</div>
+						<div className='w-10 h-10 rounded-lg flex items-center justify-center bg-fuchsia-500 shadow-lg'>
+							<span>
+								<Browser name="Three" destiny="/" />
+							</span>
+						</div>
 					</div>
 				</div>
 			</main>
